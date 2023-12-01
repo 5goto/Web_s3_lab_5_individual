98e9519192d94b67923f4d63150d8644bc64bef0
5goto <2breal1984@gmail.com>
2023-12-01 19:12:23 +1000

    fix: controller fix

--- app.py
+++ app.py
@@ -4,63 +4,7 @@ from unils.complex_funx import ComplexMod
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    result = False
-    form = ''
-    base = ''
-    r = ''
-
-    if request.method == "GET":
-        form = request.values.get('form')
-        return render_template('index.html',
-                               form=form, result=result)
-    else:
-        result = True
-
-        form_result = ''
-        img_result = ''
-        arg_result = ''
-
-        if 'r' in request.values:
-            r = request.values.get('r')
-            img = request.values.get('imagine')
-            operations = request.values.getlist('operation[]')
-
-            comp_mod = ComplexMod(float(r), float(img))
-
-            if '0' in operations:
-                form_result = comp_mod.get_alg_form()
-            if '1' in operations:
-                img_result = comp_mod.get_img()
-            if '2' in operations:
-                arg_result = comp_mod.get_arg()
-
-        else:
-            base = request.values.get('base')
-            img = request.values.get('imagine')
-            operations = request.values.getlist('operation[]')
-
-            comp_mod = ComplexMod(float(base), float(img))
-
-            if '0' in operations:
-                form_result = comp_mod.get_exp_form()
-            if '1' in operations:
-                img_result = comp_mod.get_img()
-            if '2' in operations:
-                arg_result = comp_mod.get_arg()
-
-        return render_template('index.html',
-                               form='alg',
-                               result=result,
-                               form_result=form_result,
-                               img_result=img_result,
-                               arg_result=arg_result,
-                               base=base,
-                               r=r,
-                               img=img,
-                               operations=operations)
-
+import controllers.index
 
 if __name__ == '__main__':
     app.run()
